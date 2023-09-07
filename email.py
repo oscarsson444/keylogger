b6f0ad52f75ccce2443da69251a55609cd0d4ba0
oscarsson444 <oscarsson444@gmail.com>
2023-09-07 20:18:49 +0200

    Sending emails with a text and an image is now possible

---
+++ email.py
@@ -0,0 +1,116 @@
+import os
+import pickle
+import sys
+
+# Gmail API utilities
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# Encoding utilities
+from base64 import urlsafe_b64encode
+
+# Email utilities
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from mimetypes import guess_type as guess_mime_type
+
+# Constants
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
+sender = sys.argv[1]  # Sender's email address passed as command line argument
+to = sys.argv[2]  # Recipient's email address passed as command line argument
+
+
+# Authenticate the user to the Gmail API
+def authenticate():
+    creds = None
+
+    # Check if token.pickle exists
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            # Refresh token
+            creds.refresh(Request())
+        else:
+            # Request new token
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Save token
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+
+    return build("gmail", "v1", credentials=creds)
+
+
+# Create a message with an embedded image
+def create_email_with_embedded_image(sender, to, subject, message_text, image_filename):
+    # Create message container
+    msg = MIMEMultipart()
+    msg["to"] = to
+    msg["from"] = sender
+    msg["subject"] = subject
+
+    # Create HTML message with embedded image
+    html = f"""\
+    <html>
+    <body>
+        <img src="cid:image1">
+        <p>{message_text}</p>
+    </body>
+    </html>
+    """
+
+    msg.attach(MIMEText(html, "html"))
+
+    # Attach the image with a Content-ID
+    with open(image_filename, "rb") as image_file:
+        image_data = image_file.read()
+        image = MIMEImage(image_data)
+        image.add_header("Content-ID", "<image1>")
+        msg.attach(image)
+
+    return msg
+
+
+# Create a raw email message from the MIME message
+def create_raw_email_message(message):
+    return {"raw": urlsafe_b64encode(message.as_bytes()).decode()}
+
+
+# Send email with embedded image
+def send_email_with_embedded_image(sender, to, subject, message_text, image_filename):
+    # Get Gmail API service
+    service = authenticate()
+
+    # Create message with embedded image
+    email_message = create_email_with_embedded_image(
+        sender, to, subject, message_text, image_filename
+    )
+
+    # Convert the MIME message to a raw email message
+    raw_email_message = create_raw_email_message(email_message)
+
+    # Send message
+    try:
+        message = (
+            service.users()
+            .messages()
+            .send(userId="me", body=raw_email_message)
+            .execute()
+        )
+        print("Message Id: %s" % message["id"])
+        return message
+    except Exception as error:
+        print("An error occurred: %s" % error)
+        return None
+
+
+if __name__ == "__main__":
+    send_email_with_embedded_image(sender, to, "Keylog", "Hello World", "devil.png")
